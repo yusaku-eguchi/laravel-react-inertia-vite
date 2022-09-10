fd831d6f0eb0f8bd584a2c25f6de250322e37f24
yusaku-eguchi <yusaku.eguchi@sharing-innovations.co.jp>
2022-09-10 22:53:33 +0900

    パスワードリセットメールを日本語化した

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 // use Illuminate\Support\Facades\Gate;
 
 use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Notifications\Messages\MailMessage;
 
@@ -34,6 +35,15 @@ class AuthServiceProvider extends ServiceProvider
                 ->greeting('会員登録いただきありがとうございます')
                 ->line('メール確認ボタンを押下いただき、メールアドレスの確認を完了してください')
                 ->action('メールアドレスを確認する', $url);
+        });
+
+        ResetPassword::toMailUsing(function ($notifiable, $url) {
+            return (new MailMessage)
+                ->subject('パスワードの再設定')
+                ->greeting('パスワード再設定')
+                ->line('パスワード再設定ボタンを押下いただき、パスワードの再設定を行ってください')
+                ->action('パスワード再設定', $url)
+                ->line('このリンクの有効期限は60秒です。');
         });
     }
 }
